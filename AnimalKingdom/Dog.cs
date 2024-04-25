22a55126c0632babaa851a177f8f65e9a6efa7ca
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-25 14:59:04 +0100

    Add NumberofNipples + Heritage and interface

--- AnimalKingdom/Dog.cs
+++ AnimalKingdom/Dog.cs
@@ -1,10 +1,17 @@
 ﻿namespace AnimalKingdom
 {
-    public class Dog
+    public class Dog : Animal, IMammal
     {
         public override string Sound()
         {
             return base.Sound() + "Woof!";
+        }
+
+        public int NumberOfNipples { get; }
+
+        public Dog()
+        {
+            NumberOfNipples = 8;
         }
     }
 }
