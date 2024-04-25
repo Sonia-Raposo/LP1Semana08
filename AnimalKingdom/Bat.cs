ccafe3d4a97124ed27d0b7c376000fd222e16c44
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-25 15:03:54 +0100

    Add NumberofNipples and NumberOfWings + Heritage and interface

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -1,10 +1,20 @@
 ﻿namespace AnimalKingdom
 {
-    public class Bat
+    public class Bat : Animal, IMammal, ICanFly
     {
         public override string Sound()
         {
             return base.Sound() + "Eek!";
+        }
+
+        public int NumberOfNipples { get; }
+        public int NumberOfWings { get; }
+
+        public Bat()
+        {
+            NumberOfNipples = 2;
+
+            NumberOfWings = 2;
         }
     }
 }
