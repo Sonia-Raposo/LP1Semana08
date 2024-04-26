bc3d60fbea84578d7205ea19fb1765d610a44dec
Sonia-Raposo <a22000344@uhlt.alunos.pt>
2024-04-26 14:03:37 +0100

    Create IComparer<Player>

---
+++ PlayerManager4/CompareByName.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager4
+{
+    public class CompareByName: IComparer <Player>
+    {
+
+        private bool order { get; }
+        
+        public class CompareByName
+        {
+            public int Compare()
+            {
+
+            }
+
+        }
+
+    }
+}
